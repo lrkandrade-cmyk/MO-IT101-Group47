26578176437eee61e55fc8dc789703bdd569b468
jyvy <lr.jkaycardo@mmdc.mcl.edu.ph>
2026-03-13 19:13:22 +0800

    Removed readEmployees method and implemented payroll and employee menu; payroll temporarily disabled for testing

--- src/Main.java
+++ src/Main.java
@@ -11,57 +11,124 @@ public class Main {
     public static void main(String[] args) {
 
 
-                login();   // start the program
+        login();   // start the program
 
+    }
+
+    static void login() {
+
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.print("Username: ");
+        String username = sc.nextLine();
+
+        System.out.print("Password: ");
+        String password = sc.nextLine();
+/*
+        if(username.equals("payroll_staff") && password.equals("12345")) {
+            System.out.println("Login Successful");
+            payrollMenu();
+                }
+*/
+
+        if (username.equals("employee") && password.equals("12345")) {
+            employeeMenu();
+        } else {
+            System.out.println("Incorrect username and/or password");
+            System.exit(0);
+        }
+    }
+
+    /*  // Shows menu for payroll staff
+    static void payrollMenu() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n1 Process Payroll");
+        System.out.println("2 Exit Program");
+        System.out.println("Choice");
+        int choice = sc.nextInt();
+        if (choice == 1) {
+            payrollMenu();
+        } else if (choice == 2) {
+            System.exit(0);
+        }
+    }
+    /*static void processPayroll(){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("\n1 One Employee");
+            System.out.println("2 All Employees");
+            System.out.println("3 Exit Program");
+            System.out.println("Choice: ");
+            int choice = sc.nextInt();
+            if (choice == 1) {
+                payrollOneEmployee();
             }
-
-            static void login(){
-
-                Scanner sc = new Scanner(System.in);
-
-                String username = "admin";
-                String password = "1234";
-
-                System.out.print("Username: ");
-                String user = sc.nextLine();
-
-                System.out.print("Password: ");
-                String pass = sc.nextLine();
-
-                if(user.equals(username) && pass.equals(password)){
-                    System.out.println("Login Successful");
-                    menu();
-                }else{
-                    System.out.println("Wrong login");
+            if (choice == 2) {
+                payrollALlEmployees();
+            else if (choice == 3) {
+                System.exit(0);
                 }
-
             }
+        }
+        */
 
-            static void menu(){
+    static void employeeMenu() {
 
-                Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-                System.out.println("\n1 View Employees");
-                System.out.println("2 Compute Payroll");
-                System.out.println("3 Exit");
+        System.out.println("\n1 Enter Employee Number ");
+        System.out.println("2 Exit Program");
+        int choice = sc.nextInt();
+        if (choice == 1) {
+            enterEmployeeNumber();
+        } else if (choice == 2) {
+            System.exit(0);
+        }
+    }
+    // asks for employee number
+    static void enterEmployeeNumber() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Employee Number : ");
+        String employeeNumber = sc.nextLine();
 
-                System.out.print("Choice: ");
-                int choice = sc.nextInt();
+        searchEmployee(employeeNumber);
+    }
+    //reads file,searches and displays employee info
+    static void searchEmployee(String empNum) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("employee.csv"));
+            String line;
+            boolean found = false;
+            br.readLine();
+            while ((line = br.readLine()) != null) {
+                line = line.replaceAll("\"", "");
 
-                if(choice == 1){
-                    readEmployees();
+                String[] data = line.split(",");
+
+                String id = data[0];
+                String lastname = data[1];
+                String firstname = data[2];
+                String birthdate = data[3];
+                if (id.equals(empNum)) {
+                    System.out.println("\nEmployee Number : " + id);
+                    System.out.println("Employee Name : " + firstname + " " + lastname);
+                    System.out.println("Employee Birth Date : " + birthdate);
+                    found = true;
+                    break;
                 }
-                else if(choice == 2){
-                    computePayroll();
+            }
+            if (!found) {
+                System.out.println("Employee Number does not exist");
+            }
+            br.close();
+        } catch (IOException e) {
+            System.out.println("Error reading file");
+        }
+    }
+    /*
+   static void computePayroll() {
 
-                }
-                else if(choice == 3){
-                    System.exit(0);
-                }
-
-            } static void computePayroll(){
-
-        try{
+        try {
 
             BufferedReader br = new BufferedReader(new FileReader("attendance.csv"));
 
@@ -70,9 +137,9 @@ public class Main {
             br.readLine(); // skip header
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("H:mm");
 
-            while((line = br.readLine()) != null){
+            while ((line = br.readLine()) != null) {
 
-                line = line.replace("\"","");
+                line = line.replace("\"", "");
 
                 String[] data = line.split(",");
 
@@ -101,73 +168,11 @@ public class Main {
 
             br.close();
 
-        }
-        catch(IOException e){
+        } catch (IOException e) {
             System.out.println("Error reading attendance file.");
         }
 
     }
+*/
 
-        static void readEmployees() {
-
-            try {
-
-                BufferedReader br = new BufferedReader(new FileReader("employee.csv"));
-
-                String line;
-
-                br.readLine(); // skip header
-
-                while ((line = br.readLine()) != null) {
-
-                    line = line.replace("\"", "");
-
-                    String[] data = line.split(",");
-
-                    String id = data[0];
-                    String lastname = data[1];
-                    String firstname = data[2];
-
-                    System.out.println(id + " - " + firstname + " " + lastname);
-
-                }
-
-                br.close();
-            }
-            catch (IOException e) {
-                System.out.println("Error reading employee file.");
-            }
-
-
-
-
-                try {
-
-                    BufferedReader br = new BufferedReader(new FileReader("employee.csv"));
-
-                    String line;
-
-                    br.readLine(); // skip header
-
-                    while ((line = br.readLine()) != null) {
-
-                        line = line.replace("\"", "");
-
-                        String[] data = line.split(",");
-
-                        String id = data[0];
-                        String lastname = data[1];
-                        String firstname = data[2];
-
-                        System.out.println(id + " - " + firstname + " " + lastname);
-
-                    }
-
-                    br.close();
-
-                } catch (IOException e) {
-                    System.out.println("Error reading employee file.");
-                }
-
-            }
-        }
+}
